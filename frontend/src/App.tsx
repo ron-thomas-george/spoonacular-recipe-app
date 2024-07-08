92fe2415d75f0f9b9f7b8a6faba97649605b6a93
Ron Thomas George <ronthomasgeorge@Rons-MacBook-Air.local>
2024-07-08 15:09:43 +0530

    Added Pagination to View More Recipes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,32 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import "./App.css";
 import searchRecipes from "./API";
 import { Recipe } from "./types";
 import RecipeCard from "./components/RecipeCard";
-
 function App() {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const pageNumber = useRef(0);
 
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault();
     try {
       const { results } = await searchRecipes(searchTerm, 1);
       setRecipes(results);
+      pageNumber.current = 1;
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const handleViewMoreClick = async () => {
+    try {
+      const nextPage = pageNumber.current + 1;
+      const nextRecipes = await searchRecipes(searchTerm, nextPage);
+      setRecipes((prevRecipes) => [...prevRecipes, ...nextRecipes.results]);
+      pageNumber.current = nextPage;
+    } catch (error) {}
   };
   return (
     <>
@@ -32,6 +43,7 @@ function App() {
       {recipes.map((recipe: Recipe) => (
         <RecipeCard key={recipe.id} recipe={recipe} />
       ))}
+      <button onClick={handleViewMoreClick}>View More</button>
     </>
   );
 }
